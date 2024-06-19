73fe67b2cc35c91f76a2aaaecd3fd82064d75521
bkwlk <kowiel.z.bartosz@gmail.com>
2024-06-19 15:08:57 +0000

    add init script

---
+++ init.sh
@@ -0,0 +1,39 @@
+PACKAGE_FILE="packages"
+
+# Check if the file exists
+if [[ ! -f "$PACKAGE_FILE" ]]; then
+  echo "Package file $PACKAGE_FILE not found!"
+  exit 1
+fi
+
+# Read the package file and install each package
+while IFS= read -r package; do
+  if [[ -n "$package" ]]; then
+    echo "Installing $package..."
+    sudo apt-get install -y "$package"
+  fi
+done < "$PACKAGE_FILE"
+
+TMUX_VERSION="3.4"
+mkdir -p "$HOME/.zsh"
+mkdir -p "$HOME/.local/bin"
+chsh -s $(which zsh)
+# Install Neovim
+# https://github.com/neovim/neovim/blob/master/INSTALL.md#linux
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -o /tmp/nvim.tar.gz && \
+sudo rm -rf /opt/nvim && \
+sudo mkdir -p /opt/nvim && \
+sudo tar -C /opt/nvim -xzf /tmp/nvim.tar.gz --strip-components=1
+ln -s /opt/nvim/bin/nvim ~/.local/bin/nvim
+
+echo "Add neovim bin location to path!"
+# echo 'echo \'export PATH="$PATH:/opt/nvim/bin"\' >> ~/.bashrc'
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+
+
+# FZF install
+# https://github.com/junegunn/fzf?tab=readme-ov-file#using-git
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --no-bash --no-fish --xdg --all --key-bindings --completion --no-update-rc
